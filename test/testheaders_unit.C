4c4d67b87a059006cb61d5ed0cdc62879210cd34
roystgnr <roystgnr@0bd85f0b-c151-43b9-a1e9-27231104551f>
2013-11-12 23:33:08 +0000

    Uncommenting this when testing NamedIndexArray

--- test/testheaders_unit.C
+++ test/testheaders_unit.C
@@ -2,6 +2,7 @@
 #include "metaphysicl/dualshadowsparsestruct.h"
 #include "metaphysicl/dualshadowsparsevector.h"
 #include "metaphysicl/dualshadowvector.h"
+// #include "metaphysicl/namedindexarray.h"
 
 int main (void)
 {
