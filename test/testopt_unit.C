daf06494b4af9aa0bb931963bda16c2ba54e5f82
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-11-14 23:13:09 -0600

    More test cases

--- test/testopt_unit.C
+++ test/testopt_unit.C
@@ -34,8 +34,10 @@ int main(void)
   std::cos(NV);
 
   SparseNumberVectorUnitVector<3, 2, float>::type SNV;
+  SparseNumberVectorUnitVector<3, 1, float>::type SNV2;
   std::sin(SNV);
   std::max(SNV, SNV);
+  std::max(SNV, SNV2);
   std::max(1.0, SNV);
   std::max(SNV, 1);
 
@@ -44,4 +46,22 @@ int main(void)
   std::max(SNS, SNS);
   std::max(1.0, SNS);
   std::max(SNS, 1);
+
+  const SparseNumberVector<unsigned int,
+    Container<UnsignedIntType<0u, NullType>,
+      Container<UnsignedIntType<1u, NullType>,
+        Container<UnsignedIntType<5u, NullType>,
+        NullContainer, ValueLessThan>,
+      ValueLessThan>,
+    ValueLessThan> > max_test_1 = 0;
+
+  const SparseNumberVector<unsigned int,
+    Container<UnsignedIntType<1u, NullType>,
+      Container<UnsignedIntType<2u, NullType>,
+        Container<UnsignedIntType<6u, NullType>,
+        NullContainer, ValueLessThan>,
+      ValueLessThan>,
+    ValueLessThan> > max_test_2 = 0;
+
+  std::max(max_test_1, max_test_2);
 }
