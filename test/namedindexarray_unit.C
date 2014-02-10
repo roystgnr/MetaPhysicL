dab1b6c32d8ee1031bf7dd5860f5937d23d49a6c
Roy H. Stogner <roystgnr@ices.utexas.edu>
2014-02-10 15:10:15 -0600

    Some tests using VexCL

--- test/namedindexarray_unit.C
+++ test/namedindexarray_unit.C
@@ -1,8 +1,17 @@
 
-#include <iostream>
-
+// MetaPhysicL
 #include "metaphysicl/sparsenumbervector.h"
 #include "metaphysicl/namedindexarray.h"
+
+#include "metaphysicl_config.h"
+
+// VexCL
+#ifdef METAPHYSICL_HAVE_VEXCL
+#include "vexcl/vexcl.hpp"
+#endif
+
+// C++
+#include <iostream>
 
 using namespace MetaPhysicL;
 
@@ -19,6 +28,49 @@ int main(void)
   indexed_by_three test_val;
   test_val.raw_data() = 1;
   test_val.raw_sizes().get<3>() = 1;
+
+#ifdef METAPHYSICL_HAVE_VEXCL
+  vex::Context ctx (vex::Filter::Env && vex::Filter::Count(1));
+  std::cout << ctx << std::endl;
+      
+
+  typedef
+    NamedIndexArray
+      <vex::vector<double>,
+       SparseNumberVector
+         <long unsigned int,
+          ULongSetConstructor<2>::type> >
+    vex_indexed_by_two;
+
+  typedef
+    NamedIndexArray
+      <vex::vector<double>,
+       SparseNumberVector
+         <long unsigned int,
+          ULongSetConstructor<1>::type> >
+    vex_indexed_by_one;
+
+  vex_indexed_by_one test_one(vex::vector<double>(ctx, 5),0);
+  test_one.raw_sizes().template get<1>() = 5;
+  vex_indexed_by_two test_two(vex::vector<double>(ctx, 3),0);
+  test_two.raw_sizes().template get<2>() = 3;
+
+  test_one.raw_data()[2] = 7;
+  test_two.raw_data()[1] = 2;
+
+  auto test_three = test_one * test_two;
+
+  if (test_three.raw_sizes().template get<1>() != 5)
+    return 1;
+
+  if (test_three.raw_sizes().template get<2>() != 3)
+    return 1;
+
+  vex::vector<double> test_output = test_three.raw_data();
+
+  if (test_output[7] != 14)
+    return 1;
+#endif
 
   return 0;
 }
