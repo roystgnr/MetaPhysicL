5419f51325ca5a2beaa0770238465e458adbebfa
Roy H. Stogner <roystgnr@ices.utexas.edu>
2014-02-17 13:36:28 -0600

    More DualExpression, NamedIndexArray tests

--- test/dualnamedarray_unit.C
+++ test/dualnamedarray_unit.C
@@ -30,12 +30,15 @@ namespace MetaPhysicL {
 #include "metaphysicl/sparsenumbervector.h"
 #include "metaphysicl/dualnamedarray.h"
 
-#include "metaphysicl_config.h"
+#include "metaphysicl/metaphysicl_asserts.h"
 
 // C++
 #include <iostream>
 
 using namespace MetaPhysicL;
+
+#define test_assert_equal_to(expr1,expr2)  do { if (!(expr1 == expr2)) { std::cerr << "Assertion `" #expr1 " == " #expr2 "' failed.\n" #expr1 " = " << (expr1) << "\n" #expr2 " = " << (expr2) << std::endl; metaphysicl_error(); } } while(0)
+
 
 int main(void)
 {
@@ -89,42 +92,110 @@ int main(void)
   typedef
     DualExpression<vex_indexed_by_one, vex_indexed_by_one> dual_one;
 
-  vex_indexed_by_one test_one_val(vex::vector<double>(ctx, 5),0);
-  test_one_val.raw_sizes().template get<1>() = 5;
+  double zeros[5];
+  for (unsigned int i = 0; i != 5; ++i)
+    zeros[i] = 0;
 
-  vex_indexed_by_one test_one_deriv(vex::vector<double>(ctx, 5),0);
+  vex_indexed_by_one test_one_val(vex::vector<double>(ctx, 5, zeros),0);
+  test_one_val.raw_sizes().template get<1>() = 5;
+  test_one_val.raw_data()[2] = 7;
+
+  vex_indexed_by_one test_one_deriv(vex::vector<double>(ctx, 5, zeros),0);
   test_one_deriv.raw_sizes().template get<1>() = 5;
 
   dual_one test_one(test_one_val, test_one_deriv);
 
-  vex_indexed_by_two test_two_val(vex::vector<double>(ctx, 3),0);
+  vex_indexed_by_two test_two_val(vex::vector<double>(ctx, 3, zeros),0);
   test_two_val.raw_sizes().template get<2>() = 3;
+  test_two_val.raw_data()[1] = 2;
 
-  vex_indexed_by_two test_two_deriv(vex::vector<double>(ctx, 3),0);
+  vex_indexed_by_two test_two_deriv(vex::vector<double>(ctx, 3, zeros),0);
   test_two_deriv.raw_sizes().template get<2>() = 3;
 
   dual_two test_two(test_two_val, test_two_deriv);
 
-  test_one.value().raw_data()[2] = 7;
-  test_two.value().raw_data()[1] = 2;
+  test_assert_equal_to (test_one.value().raw_data()[2], 7);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_one.value().raw_data()[i], 0);
+
+  test_assert_equal_to (test_two.value().raw_data()[1], 2);
+  for (unsigned int i = 0; i != 3; ++i)
+    if (i != 1)
+      test_assert_equal_to (test_two.value().raw_data()[i], 0);
 
   auto test_three_val_val = test_one_val * test_two_val;
   auto test_three_deriv_val = test_one_deriv * test_two_val;
 
+  std::cout << "test_one_val = " << test_one_val << std::endl;
+  test_assert_equal_to (test_one_val.raw_data()[2], 7);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_one_val.raw_data()[i], 0);
+
+  auto test_three_ag = (test_one_val * test_one_val);
+  vex::vector<double> test_output_three_ag = test_three_ag.raw_data();
+  std::cout << "test_output_three_ag = " << test_output_three_ag << std::endl;
+  test_assert_equal_to (test_output_three_ag[2], 49);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_three_ag[i], 0);
+
+  auto test_three_am = (test_one * test_one_val);
+  vex::vector<double> test_output_three_am = test_three_am.value().raw_data();
+  std::cout << "test_output_three_am = " << test_output_three_am << std::endl;
+  test_assert_equal_to (test_output_three_am[2], 49);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_three_am[i], 0);
+
   auto test_three_a = test_one * test_two_val;
+  test_assert_equal_to (test_three_a.value().raw_sizes().template get<1>(), 5);
+  test_assert_equal_to (test_three_a.value().raw_sizes().template get<2>(), 3);
+  vex::vector<double> test_output_three_a = test_three_a.value().raw_data();
+  std::cout << "test_output_three_a = " << test_output_three_a << std::endl;
+  test_assert_equal_to (test_output_three_a[7], 14);
+  for (unsigned int i = 0; i != 15; ++i)
+    if (i != 7)
+      test_assert_equal_to (test_output_three_a[i], 0);
 
   auto test_three = test_one * test_two;
+  test_assert_equal_to (test_three.value().raw_sizes().template get<1>(), 5);
+  test_assert_equal_to (test_three.value().raw_sizes().template get<2>(), 3);
+  vex::vector<double> test_output_three = test_three.value().raw_data();
+  test_assert_equal_to (test_output_three[7], 14);
+  for (unsigned int i = 0; i != 15; ++i)
+    if (i != 7)
+      test_assert_equal_to (test_output_three[i], 0);
 
-  if (test_three.value().raw_sizes().template get<1>() != 5)
-    return 1;
+  auto test_four = test_one / 2.0;
+  vex::vector<double> test_output_four = test_four.value().raw_data();
+  test_assert_equal_to (test_output_four[2], 3.5);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_four[i], 0);
 
-  if (test_three.value().raw_sizes().template get<2>() != 3)
-    return 1;
+  auto test_five = test_one * test_one;
+  vex::vector<double> test_output_five = test_five.value().raw_data();
+  test_assert_equal_to (test_output_five[2], 49);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_five[i], 0);
 
-  vex::vector<double> test_output = test_three.value().raw_data();
+  auto test_six = test_five / 2.0;
+  vex::vector<double> test_output_six = test_six.value().raw_data();
+  test_assert_equal_to (test_output_six[2], 24.5);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_six[i], 0);
 
-  if (test_output[7] != 14)
-    return 1;
+  auto test_seven = (test_one * test_one_val) / 2.0;
+  vex::vector<double> test_output_seven = test_seven.value().raw_data();
+  test_assert_equal_to (test_output_seven[2], 24.5);
+  for (unsigned int i = 0; i != 5; ++i)
+    if (i != 2)
+      test_assert_equal_to (test_output_seven[i], 0);
+
 #endif
 
   return 0;
