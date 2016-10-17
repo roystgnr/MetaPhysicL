7f48abea522d1ee0ae73940d5222da4cf7d8de7a
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-10-17 17:19:49 -0500

    Don't test C++11 code unless we have C++11

--- test/instantiations_unit.C
+++ test/instantiations_unit.C
@@ -1,19 +1,26 @@
 
-#include "metaphysicl/dualnamedarray.h"
+#if __cplusplus >= 201103L
+#  include "metaphysicl/dualnamedarray.h"
+#  include "metaphysicl/namedindexarray.h"
+#endif
+
 #include "metaphysicl/dualnumberarray.h"
 #include "metaphysicl/dualshadowsparsestruct.h"
 #include "metaphysicl/dualshadowsparsevector.h"
 #include "metaphysicl/dualshadowvector.h"
 #include "metaphysicl/dynamicsparsenumberarray.h"
 #include "metaphysicl/dynamicsparsenumbervector.h"
-#include "metaphysicl/namedindexarray.h"
 #include "metaphysicl/sparsenumberarray.h"
 
 using namespace MetaPhysicL;
 
 template <typename T1, typename T2>
 struct Instantiator {
+
+#if __cplusplus >= 201103L
   DualExpression<T1, T2> test_de;
+#endif
+
   DualNumber<T1, T2> test_dn;
   ShadowNumber<T1, T2> test_shadow;
   NumberArray<5, T1> test_na;
--- test/testheaders_unit.C
+++ test/testheaders_unit.C
@@ -1,9 +1,12 @@
 
-#include "metaphysicl/dualexpression.h"
 #include "metaphysicl/dualshadowsparsestruct.h"
 #include "metaphysicl/dualshadowsparsevector.h"
 #include "metaphysicl/dualshadowvector.h"
-#include "metaphysicl/namedindexarray.h"
+
+#if __cplusplus >= 201103L
+#  include "metaphysicl/dualexpression.h"
+#  include "metaphysicl/namedindexarray.h"
+#endif
 
 int main (void)
 {
