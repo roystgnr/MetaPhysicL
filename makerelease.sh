8e1892c22229987f0c5be0877e4ee6fcf79ef5fc
Roy Stogner <roy@stogners.org>
2018-09-07 10:01:22 -0500

    Script for making new release tags

---
+++ makerelease.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+if [ "$#" -ne 0 ]; then
+  echo "Usage: $0"
+fi
+
+VERSION=$(grep AC_INIT configure.ac | sed 's/.*[ ,]\([0-9]\+\(\.[0-9]\+\)\+\).*/\1/')
+
+git tag v${VERSION} -m "MetaPhysicL $VERSION release"
+
+git checkout -b branch_$VERSION
+
+perl -pi -e 's/dnl AM_MAINTAINER_MODE/AM_MAINTAINER_MODE/' configure.ac
+
+./bootstrap
+
+cp .gitignore-bootstrapped .gitignore
+
+git add Makefile.in aclocal.m4 metaphysicl_config.h.tmp.in build-aux \
+        configure doxygen/Makefile.in m4/libtool.m4 m4/ltoptions.m4 \
+        m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4 \
+        src/Makefile.in test/Makefile.in configure.ac
+
+git commit -m "Add bootstrap output"
+
+git tag v${VERSION}_bootstrapped -m "MetaPhysicL $VERSION release, bootstrapped"
